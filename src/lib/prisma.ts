b936e69a5ab25ee7aec6ef1df38e99f2ce4ea3d7
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-12-24 15:38:13 +0545

    Setting Up Prisma with PostgresSQL

---
+++ src/lib/prisma.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+const prisma = new PrismaClient({
+  adapter: new PrismaPg(pool),
+});
+
+export default prisma;
